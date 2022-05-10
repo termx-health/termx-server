21f6671b79592435d52fe9badcba1e1bac9139c2
Marina Ivanova <marina.ivanova@kodality.com>
2022-05-10 16:28:23 +0300

    concept query

--- termserver-service/src/main/java/com/kodality/termserver/ts/codesystem/concept/ConceptController.java
+++ termserver-service/src/main/java/com/kodality/termserver/ts/codesystem/concept/ConceptController.java
@@ -1,7 +1,9 @@
 package com.kodality.termserver.ts.codesystem.concept;
 
 import com.kodality.commons.exception.NotFoundException;
+import com.kodality.commons.model.QueryResult;
 import com.kodality.termserver.codesystem.Concept;
+import com.kodality.termserver.codesystem.ConceptQueryParams;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
@@ -17,4 +19,10 @@ public class ConceptController {
   public Concept getConcept(@PathVariable Long id) {
     return conceptService.get(id).orElseThrow(() -> new NotFoundException("Concept not found: " + id));
   }
+
+  @Get(uri = "{?params*}")
+  public QueryResult<Concept> getConcepts(ConceptQueryParams params) {
+    return conceptService.query(params);
+  }
+
 }
