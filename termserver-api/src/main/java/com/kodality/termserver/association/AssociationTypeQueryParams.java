cd6bd068898d2014875513ecded8e7ce20e254c7
ViktorUst <viktor.ustits@kodality.com>
2022-07-05 16:10:51 +0300

    merge request fixes

--- termserver-api/src/main/java/com/kodality/termserver/association/AssociationTypeQueryParams.java
+++ termserver-api/src/main/java/com/kodality/termserver/association/AssociationTypeQueryParams.java
@@ -1,4 +1,4 @@
-package com.kodality.termserver.mapset;
+package com.kodality.termserver.association;
 
 import com.kodality.commons.model.QueryParams;
 import lombok.Getter;
@@ -8,7 +8,7 @@ import lombok.experimental.Accessors;
 @Getter
 @Setter
 @Accessors(chain = true)
-public class MapSetAssociationTypeQueryParams extends QueryParams {
+public class AssociationTypeQueryParams extends QueryParams {
   private String code;
   private String codeContains;
 }
--- termserver-service/src/main/java/com/kodality/termserver/ts/mapset/association/MapSetAssociationController.java
+++ termserver-service/src/main/java/com/kodality/termserver/ts/mapset/association/MapSetAssociationController.java
@@ -1,10 +1,7 @@
 package com.kodality.termserver.ts.mapset.association;
 
 import com.kodality.commons.exception.NotFoundException;
-import com.kodality.commons.model.QueryResult;
-import com.kodality.termserver.association.AssociationType;
 import com.kodality.termserver.mapset.MapSetAssociation;
-import com.kodality.termserver.mapset.MapSetAssociationTypeQueryParams;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
@@ -19,10 +16,5 @@ public class MapSetAssociationController {
   @Get(uri = "/{id}")
   public MapSetAssociation getAssociation(@PathVariable Long id) {
     return associationService.get(id).orElseThrow(() -> new NotFoundException("Map set association not found: " + id));
-  }
-
-  @Get(uri = "/types{?params*}")
-  public QueryResult<AssociationType> queryAssociationTypes(MapSetAssociationTypeQueryParams params) {
-    return associationService.queryAssociationTypes(params);
   }
 }
