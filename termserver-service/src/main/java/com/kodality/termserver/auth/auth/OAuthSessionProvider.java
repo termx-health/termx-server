4c3936390a9fc8ef53d6db72d5764c7cb103bf39
Daniel <daniel@kodality.com>
2022-07-19 10:59:30 +0300

    oauth

--- termserver-service/src/main/java/com/kodality/termserver/auth/auth/OAuthSessionProvider.java
+++ termserver-service/src/main/java/com/kodality/termserver/auth/auth/OAuthSessionProvider.java
@@ -1,7 +1,9 @@
 package com.kodality.termserver.auth.auth;
 
 import com.kodality.commons.cache.CacheManager;
+import com.kodality.commons.client.HttpClient;
 import com.kodality.commons.util.JsonUtil;
+import io.micronaut.context.annotation.Value;
 import io.micronaut.http.HttpRequest;
 import io.micronaut.http.cookie.Cookie;
 import java.util.Base64;
@@ -12,20 +14,20 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Singleton
 public class OAuthSessionProvider extends SessionProvider{
-
   private static final String AUTHORIZATION = "Authorization";
   public static final String OAUTH_TOKEN_COOKIE = "oauth-token";
 
+  private final HttpClient authClient;
   private final CacheManager tokenCache = new CacheManager();
-  private final Base64.Decoder decoder = Base64.getUrlDecoder();
 
-  public OAuthSessionProvider() {
+  public OAuthSessionProvider(@Value("${auth.oauth.userinfo}") String userinfoUrl) {
     tokenCache.initCache("tokens", 1000, 5);
+    this.authClient = new HttpClient(userinfoUrl);
   }
 
   @Override
   public int getOrder() {
-    return 2;
+    return 50;
   }
 
   @Override
@@ -40,11 +42,13 @@ public class OAuthSessionProvider extends SessionProvider{
 
   private SessionInfo getSessionInfo(String auth) {
     return tokenCache.getCf("tokens", auth, () -> {
-      log.debug("Querying user info from SSO with authorization header: {}", auth);
-      String[] chunks = auth.split("\\.");
-      String payload = new String(decoder.decode(chunks[1]));
-      SessionInfo sessionInfo = JsonUtil.fromJson(payload, SessionInfo.class);
-      return CompletableFuture.completedFuture(sessionInfo);
+      java.net.http.HttpRequest req = authClient.builder("/").GET().headers(AUTHORIZATION, auth).build();
+      return authClient.executeAsync(req)
+          .thenApply(resp -> JsonUtil.fromJson(resp.body(), SessionInfo.class))
+          .exceptionally(e -> {
+            log.warn("Failed to get SessionInfo from SSO with authorization header: {}. Exception: {}", auth, e.getMessage());
+            return null;
+          });
     }).join();
   }
 
