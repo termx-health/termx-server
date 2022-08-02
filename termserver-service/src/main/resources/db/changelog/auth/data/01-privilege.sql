46db8f68ee07430812a7c6c41ebbaa330f608f36
Juri Antonov <juri.antonov@kodality.com>
2022-08-02 19:23:15 +0300

    OP-4766 default privileges

--- termserver-service/src/main/resources/db/changelog/auth/data/01-privilege.sql
+++ termserver-service/src/main/resources/db/changelog/auth/data/01-privilege.sql
@@ -1,16 +1,11 @@
 --liquibase formatted sql
 
---changeset kodality:privileges
-with t (code, names) as (values ('admin', '{"en": "Admin"}'),
-                             ('kodality.code-system.view', '{"en": "Code system view"}'),
-                             ('kodality.code-system.edit', '{"en": "Code system edit"}'),
-                             ('kodality.code-system.publish', '{"en": "Code system publish"}'),
-                             ('kodality.value-set.view', '{"en": "Value set view"}'),
-                             ('kodality.value-set.edit', '{"en": "Value set edit"}'),
-                             ('kodality.value-set.publish', '{"en": "Value set publish"}'),
-                             ('kodality.map-set.view', '{"en": "Map set view"}'),
-                             ('kodality.map-set.edit', '{"en": "Map set edit"}'),
-                             ('kodality.map-set.publish', '{"en": "Map set publish"}')
+--changeset kodality:default_privileges
+with t (code, names) as (values
+                             ('kts-viewer', '{"en": "Provides read access to all resources in the terminology server"}'),
+                             ('kts-editor', '{"en": "Gives the permission to view, create and modify all resources, incl import ability."}'),
+                             ('kts-publisher', '{"en": "Same as editor with permission to publish resources."}'),
+                             ('kts-admin', '{"en": "The user can perform all actions in the application."}')
 )
    , e as (select t.*, (exists(select 1 from auth.privilege p where t.code = p.code)) as pexists from t)
    , inserted as (insert into auth.privilege(code, names) select e.code, e.names::jsonb from e where e.pexists = false)
@@ -18,37 +13,18 @@ with t (code, names) as (values ('admin', '{"en": "Admin"}'),
 select 1;
 --rollback select 1;
 
---changeset kodality:privilege-resources
-with res(resource_id, resource_type, code) as (values ('publication-status', 'CodeSystem', 'code-system'),
-                                                         ('codesystem-content-mode', 'CodeSystem', 'code-system'),
-                                                         ('v3-ietf3066', 'CodeSystem', 'code-system'),
-                                                         ('concept-property-type', 'CodeSystem', 'code-system'),
-                                                         ('contact-point-system', 'CodeSystem', 'code-system'),
-                                                         ('contact-point-use', 'CodeSystem', 'code-system'),
-                                                         ('filter-operator', 'CodeSystem', 'code-system'),
-                                                         ('namingsystem-identifier-type', 'CodeSystem', 'code-system'),
-                                                         ('namingsystem-type', 'CodeSystem', 'code-system'),
-
-                                                         ('publication-status', 'ValueSet', 'value-set'),
-                                                         ('codesystem-content-mode', 'ValueSet', 'value-set'),
-                                                         ('languages', 'ValueSet', 'value-set'),
-                                                         ('concept-property-type', 'ValueSet', 'value-set'),
-                                                         ('contact-point-system', 'ValueSet', 'value-set'),
-                                                         ('contact-point-use', 'ValueSet', 'value-set'),
-                                                         ('filter-operator', 'ValueSet', 'value-set'),
-                                                         ('namingsystem-identifier-type', 'ValueSet', 'value-set'),
-                                                         ('namingsystem-type', 'ValueSet', 'value-set')
+--changeset kodality:default_privilege_resources
+with t(privilege_code, resource_type, actions) as (values
+    ('kts-viewer', 'Any', '{"view": true}'::jsonb),
+    ('kts-editor', 'Any', '{"view": true, "edit": true}'::jsonb),
+    ('kts-publisher', 'Any', '{"view": true, "edit": true, "publish": true}'::jsonb),
+    ('kts-admin', 'Admin', null)
 )
-insert
-into auth.privilege_resource (privilege_id, resource_type, resource_id)
-select p.id, res.resource_type, res.resource_id
-from auth.privilege p, res
-where p.code like '%.' || res.code || '.%' and
-    not exists(select 1
-               from auth.privilege_resource pr
-               where pr.resource_id = res.resource_id and
-                   pr.resource_type = res.resource_type and
-                   pr.privilege_id = p.id);
+   , pr as (select * from auth.privilege p, t where p.code = t.privilege_code)
+   , e as (select pr.id as privilege_id, t.*, (exists(select 1 from auth.privilege_resource p where p.privilege_id = pr.id)) as pexists from t, pr where pr.privilege_code = t.privilege_code)
+   , inserted as (insert into auth.privilege_resource(privilege_id, resource_type, actions) select e.privilege_id, e.resource_type, e.actions from e where e.pexists = false)
+   , updated as (update auth.privilege_resource p set resource_type = e.resource_type, actions = e.actions, resource_id = null from e where e.pexists = true and e.privilege_id = p.privilege_id)
+select 1;
 --rollback select 1;
 
 
