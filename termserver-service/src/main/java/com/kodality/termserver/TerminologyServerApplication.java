c3afb4b6ee9c353e2acce669854ccd5617d9b9b7
Juri Antonov <juri.antonov@kodality.com>
2022-08-09 12:45:27 +0300

    separate swagger to standalone app

--- termserver-service/src/main/java/com/kodality/termserver/TerminologyServerApplication.java
+++ termserver-service/src/main/java/com/kodality/termserver/TerminologyServerApplication.java
@@ -2,8 +2,35 @@ package com.kodality.termserver;
 
 import io.micronaut.runtime.Micronaut;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.OAuthScope;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
-@OpenAPIDefinition
+@OpenAPIDefinition(
+    info = @Info(
+        title = "KTS",
+        description = "${openapi.description}",
+        contact = @Contact(url = "https://kodality.com", name = "Kodality support", email = "support@kodality.com")
+    )
+)
+@SecurityScheme(name = "openid",
+    type = SecuritySchemeType.OAUTH2,
+    scheme = "bearer",
+    bearerFormat = "jwt",
+    flows = @OAuthFlows(
+        authorizationCode = @OAuthFlow(
+            authorizationUrl = "${oauth.url}/protocol/openid-connect/auth",
+            tokenUrl = "${oauth.url}/protocol/openid-connect/token",
+            scopes = @OAuthScope(name = "openid", description = "OpenID scope")
+        )
+    )
+)
+@SecurityRequirement(name = "openid")
 public class TerminologyServerApplication {
 
   public static void main(String[] args) {
