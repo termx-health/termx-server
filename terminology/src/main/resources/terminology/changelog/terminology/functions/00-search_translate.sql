455a8a00ecb4c06a2dced84aa98d495b80d86629
Daniel Dubrovski <daniel@kodality.com>
2023-05-04 16:00:58 +0300

    search

--- terminology/src/main/resources/terminology/changelog/terminology/functions/00-search_translate.sql
+++ terminology/src/main/resources/terminology/changelog/terminology/functions/00-search_translate.sql
@@ -1,5 +1,3 @@
-drop function if exists terminology.search_translate;
-
 CREATE OR REPLACE FUNCTION terminology.search_translate(text) RETURNS text AS $$
   select translate(lower(regexp_replace($1, '[^\w]+',' ','g')),
                      'äąęėõöüųūįšžč',
