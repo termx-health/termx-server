94605b79b29357e4fde49356ad5528fb82c8b437
Marina Ivanova <marina.ivanova@kodality.com>
2023-05-16 12:14:37 +0300

    taskflow fix revert

--- termserver-common/src/main/java/com/kodality/termserver/taskflow/CommonTaskflowSessionProvider.java
+++ termserver-common/src/main/java/com/kodality/termserver/taskflow/CommonTaskflowSessionProvider.java
@@ -1,17 +1,17 @@
 package com.kodality.termserver.taskflow;
 
-//import com.kodality.taskflow.auth.TaskflowSessionInfo;
-//import com.kodality.taskflow.auth.TaskflowSessionProvider;
-//import com.kodality.termserver.auth.SessionStore;
-//import java.util.Optional;
+import com.kodality.taskflow.auth.TaskflowSessionInfo;
+import com.kodality.taskflow.auth.TaskflowSessionProvider;
+import com.kodality.termserver.auth.SessionStore;
+import java.util.Optional;
 import javax.inject.Singleton;
 
 @Singleton
-public class CommonTaskflowSessionProvider  {
-//public class CommonTaskflowSessionProvider implements TaskflowSessionProvider {
+//public class CommonTaskflowSessionProvider  {
+public class CommonTaskflowSessionProvider implements TaskflowSessionProvider {
 
-//  @Override
-//  public Optional<TaskflowSessionInfo> get() {
-//    return SessionStore.get().map(sessionInfo -> new TaskflowSessionInfo().setSub(sessionInfo.getUsername()).setTenant("1"));
-//  }
+  @Override
+  public Optional<TaskflowSessionInfo> get() {
+    return SessionStore.get().map(sessionInfo -> new TaskflowSessionInfo().setSub(sessionInfo.getUsername()).setTenant("1"));
+  }
 }
