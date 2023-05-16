94d199496192a65ce5683a7c1bb395e871c3989d
Marina Ivanova <marina.ivanova@kodality.com>
2023-05-16 14:39:40 +0300

    taskflow extract revert

--- termserver-common/src/main/java/com/kodality/termserver/taskflow/CommonTaskflowUserProvider.java
+++ termserver-common/src/main/java/com/kodality/termserver/taskflow/CommonTaskflowUserProvider.java
@@ -1,8 +1,7 @@
 package com.kodality.termserver.taskflow;
 
-//import com.kodality.taskflow.api.TaskflowUserProvider;
+import com.kodality.taskflow.api.TaskflowUserProvider;
 import javax.inject.Singleton;
 
 @Singleton
-public class CommonTaskflowUserProvider {}
-//public class CommonTaskflowUserProvider extends TaskflowUserProvider {}
+public class CommonTaskflowUserProvider extends TaskflowUserProvider {}
