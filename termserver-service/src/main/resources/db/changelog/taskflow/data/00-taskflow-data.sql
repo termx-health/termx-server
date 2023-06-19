fc69174290fc1b83580000093de0247f8406519e
Marina Ivanova <marina.ivanova@kodality.com>
2023-06-19 11:43:45 +0300

    version algorithm + fix

--- termserver-service/src/main/resources/db/changelog/taskflow/data/00-taskflow-data.sql
+++ termserver-service/src/main/resources/db/changelog/taskflow/data/00-taskflow-data.sql
@@ -1,15 +1,15 @@
 --liquibase formatted sql
 
---changeset kodality:taskflow.space-data
-INSERT INTO taskflow.space (institution, code, names)
+--changeset kodality:taskflow.project-data
+INSERT INTO taskflow.project (institution, code, names)
 select '1', 'kts', '{"en": "Kodality Terminology Service"}'::jsonb
-where not exists (select 1 from taskflow.space where institution = '1' and code = 'kts');
+where not exists (select 1 from taskflow.project where institution = '1' and code = 'kts');
 --
 
---changeset kodality:taskflow.workflow-data
-with t(space_id, task_type, transitions) as (
+--changeset kodality:taskflow.workflow-data-1
+with t(project_id, task_type, transitions) as (
   select
-    (select id from taskflow.space where institution = '1' and code = 'kts'),
+    (select id from taskflow.project where institution = '1' and code = 'kts'),
     'snomed-translation-validation',
     jsonb_build_array(
       '{"from": null, "to": "draft"}'::jsonb,
@@ -37,7 +37,7 @@ with t(space_id, task_type, transitions) as (
       '{"from": "cancelled", "to": "draft"}'::jsonb
     )
 )
-INSERT INTO taskflow.workflow(space_id, task_type, transitions)
-select t.space_id, t.task_type, t.transitions from t
-where not exists (select 1 from taskflow.workflow where task_type = t.task_type and space_id = t.space_id);
+INSERT INTO taskflow.workflow(project_id, task_type, transitions)
+select t.project_id, t.task_type, t.transitions from t
+where not exists (select 1 from taskflow.workflow where task_type = t.task_type and project_id = t.project_id);
 --
