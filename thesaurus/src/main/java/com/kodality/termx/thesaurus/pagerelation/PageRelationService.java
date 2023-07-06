1c041e8f382018abb0c5739caffc862939a51934
Roman Bondarev <roman.bondarev@kodality.com>
2023-07-06 10:55:54 +0300

    page relation parsing

--- thesaurus/src/main/java/com/kodality/termx/thesaurus/pagerelation/PageRelationService.java
+++ thesaurus/src/main/java/com/kodality/termx/thesaurus/pagerelation/PageRelationService.java
@@ -3,15 +3,18 @@ package com.kodality.termx.thesaurus.pagerelation;
 import com.kodality.commons.model.CodeName;
 import com.kodality.commons.model.LocalizedName;
 import com.kodality.commons.model.QueryResult;
-import com.kodality.termx.thesaurus.pagecontent.PageContentRepository;
 import com.kodality.termx.thesaurus.page.PageContent;
 import com.kodality.termx.thesaurus.page.PageRelation;
 import com.kodality.termx.thesaurus.page.PageRelationQueryParams;
+import com.kodality.termx.thesaurus.pagecontent.PageContentRepository;
 import com.kodality.termx.utils.MatcherUtil;
 import io.micronaut.core.util.CollectionUtils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.inject.Singleton;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,50 +26,79 @@ public class PageRelationService {
   private final PageContentRepository contentRepository;
 
   public List<PageRelation> loadAll(Long pageId) {
-    return repository.loadAll(pageId);
+    return repository.loadAll(pageId).stream().map(this::decorate).toList();
   }
 
   public QueryResult<PageRelation> query(PageRelationQueryParams params) {
-    QueryResult<PageRelation> query = repository.query(params);
-    query.getData().forEach(this::decorate);
-    return query;
+    return repository.query(params).map(this::decorate);
   }
 
   @Transactional
   public void save(PageContent content, Long pageId) {
-    List<PageRelation> relations = merge(extractFromContent(content), loadAll(pageId));
+    List<PageRelation> relations = merge(parseRelations(content), loadAll(pageId));
     repository.retain(relations, pageId, content.getId());
     if (CollectionUtils.isNotEmpty(relations)) {
       relations.forEach(r -> repository.save(r, pageId, content.getId()));
     }
   }
 
-  public List<PageRelation> extractFromContent(PageContent content) {
-    List<PageRelation> relations = new ArrayList<>();
-    List<String> matches = MatcherUtil.findAllMatches(content.getContent(), "\\[(.*?)\\]\\(.*?\\)");
-    matches.forEach(m -> {
-      String target = m.split("]")[1].split("\\(|\\)")[1];
-      if (!target.contains(":")) {
-        return;
-      }
-      String type = target.split(":")[0];
-      if (!List.of("cs", "vs", "ms", "concept", "page").contains(type)) {
-        return;
-      }
-      target = target.split(":")[1];
 
-      PageRelation relation = new PageRelation();
-      relation.setType(type);
-      relation.setContent(new CodeName(content.getId()));
-      relation.setTarget(target);
-      relations.add(relation);
-    });
+  private List<PageRelation> parseRelations(PageContent content) {
+    List<PageRelation> relations = new ArrayList<>();
+    relations.addAll(parseRelationLinks(content));
+    relations.addAll(parseRelationInsertions(content));
     return relations;
+  }
+
+  private List<PageRelation> parseRelationLinks(PageContent content) {
+    String regex = "\\[(.*?)]\\((.*?)\\)"; // [label](uri)
+    List<String> allowedTypes = List.of("cs", "vs", "ms", "concept", "page");
+
+    return MatcherUtil.findAllMatches(content.getContent(), regex).stream().map(m -> {
+      Matcher matcher = Pattern.compile(regex).matcher(m);
+      if (!matcher.find()){
+        return null;
+      }
+      String uri = matcher.group(2);
+      if (!uri.contains(":")) {
+        return null;
+      }
+      String type = uri.split(":")[0];
+      String target = uri.split(":")[1];
+      if (!allowedTypes.contains(type)) {
+        return null;
+      }
+      return new PageRelation()
+          .setContent(new CodeName(content.getId()))
+          .setType(type)
+          .setTarget(target);
+    }).filter(Objects::nonNull).toList();
+  }
+
+  private List<PageRelation> parseRelationInsertions(PageContent content) {
+    String regex = "\\{\\{(.*):(.*)}}"; // {{system:value}}
+    List<String> allowedSystems = List.of("def");
+
+    return MatcherUtil.findAllMatches(content.getContent(), regex).stream().map(m -> {
+      Matcher matcher = Pattern.compile(regex).matcher(m);
+      if (!matcher.find()){
+        return null;
+      }
+      String system = matcher.group(1);
+      String value = matcher.group(2);
+      if (!allowedSystems.contains(system)) {
+        return null;
+      }
+      return new PageRelation()
+          .setContent(new CodeName(content.getId()))
+          .setType(system)
+          .setTarget(value);
+    }).filter(Objects::nonNull).toList();
   }
 
   private List<PageRelation> merge(List<PageRelation> extractedRelations, List<PageRelation> currentRelations) {
     extractedRelations.forEach(extracted -> extracted.setId(currentRelations.stream().filter(current ->
-                current.getType().equals(extracted.getType()) &&
+            current.getType().equals(extracted.getType()) &&
                 current.getTarget().equals(extracted.getTarget()) &&
                 current.getContent().getId().equals(extracted.getContent().getId()))
         .findFirst().map(PageRelation::getId).orElse(null)));
@@ -75,7 +107,9 @@ public class PageRelationService {
 
   private PageRelation decorate(PageRelation relation) {
     PageContent content = contentRepository.load(relation.getContent().getId());
-    relation.getContent().setCode(content.getSlug()).setNames(new LocalizedName(Map.of(content.getLang(), content.getName())));
+    relation.getContent()
+        .setCode(content.getSlug())
+        .setNames(new LocalizedName(Map.of(content.getLang(), content.getName())));
     return relation;
   }
 }
