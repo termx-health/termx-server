be89a4db5739c742ea67c5c345e82a3045a3c9ac
Roman Bondarev <roman.bondarev@kodality.com>
2023-09-15 12:24:49 +0300

    OP-5416 vol2

--- modeler/src/main/java/com/kodality/termx/modeler/transformationdefinition/TransformerService.java
+++ modeler/src/main/java/com/kodality/termx/modeler/transformationdefinition/TransformerService.java
@@ -17,7 +17,6 @@ import java.util.stream.Collectors;
 import javax.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.collections4.CollectionUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.hl7.fhir.exceptions.FHIRException;
 import org.hl7.fhir.r5.context.ContextUtilities;
 import org.hl7.fhir.r5.elementmodel.Element;
@@ -193,20 +192,16 @@ public class TransformerService {
       List<StructureDefinition> definitions =
           definition.getResources().stream().filter(r -> r.getType().equals("definition")).map(x -> this.<StructureDefinition>parse(getContent(x))).toList();
       definitions.subList(0, definitions.size() - 1).forEach(source -> {
-        rows.add("uses \"" + source.getUrl() + "\" alias " + getAlias(source.getType()) + " as source");
+        rows.add("uses \"" + source.getUrl() + "\" alias " + source.getName() + " as source");
       });
       StructureDefinition target = definitions.get(definitions.size() - 1);
-      rows.add("uses \"" + target.getUrl() + "\" alias " + getAlias(target.getType()) + " as target");
+      rows.add("uses \"" + target.getUrl() + "\" alias " + target.getName() + " as target");
 
       rows.add("");
-      rows.add("group example(source src : " + getAlias(definitions.get(0).getType()) + ", target tgt : " + getAlias(target.getType()) + ") {");
+      rows.add("group example(source src : " + definitions.get(0).getName() + ", target tgt : " + target.getName() + ") {");
       rows.add("  ");
       rows.add("}");
     }
     return rows.stream().collect(Collectors.joining("\n"));
-  }
-
-  private String getAlias(String text){
-   return text.contains("/") ? StringUtils.substringAfterLast(text, "/") : text;
   }
 }
