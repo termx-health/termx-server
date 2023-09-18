49a6ea474f8f9fbf1226df104450c578bae8853f
Roman Bondarev <roman.bondarev@kodality.com>
2023-09-18 15:08:40 +0300

    OP-5420 duplicate transformation definition

--- modeler/src/main/java/com/kodality/termx/modeler/transformationdefinition/TransformationDefinitionService.java
+++ modeler/src/main/java/com/kodality/termx/modeler/transformationdefinition/TransformationDefinitionService.java
@@ -3,15 +3,12 @@ package com.kodality.termx.modeler.transformationdefinition;
 import com.kodality.commons.model.QueryResult;
 import jakarta.inject.Singleton;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Singleton
 public class TransformationDefinitionService {
   private final TransformationDefinitionRepository repository;
-
-  public void save(TransformationDefinition def) {
-    repository.save(def);
-  }
 
   public TransformationDefinition load(Long id) {
     return repository.load(id);
@@ -19,6 +16,19 @@ public class TransformationDefinitionService {
 
   public QueryResult<TransformationDefinition> search(TransformationDefinitionQueryParams params) {
     return repository.search(params);
+  }
+
+  public void save(TransformationDefinition def) {
+    repository.save(def);
+  }
+
+  @Transactional
+  public TransformationDefinition duplicate(Long id) {
+    TransformationDefinition def = load(id);
+    def.setId(null);
+    def.setName(def.getName() + "_duplicate");
+    save(def);
+    return def;
   }
 
   public void delete(Long id) {
