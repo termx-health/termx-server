6b6c9db08d698dc24a97819240e5cde49c7ab55d
Marina Ivanova <marina.ivanova@kodality.com>
2023-10-20 13:24:31 +0300

    lorque process auth

--- termx-core/src/main/java/com/kodality/termx/core/sys/lorque/LorqueProcessController.java
+++ termx-core/src/main/java/com/kodality/termx/core/sys/lorque/LorqueProcessController.java
@@ -1,5 +1,6 @@
 package com.kodality.termx.core.sys.lorque;
 
+import com.kodality.termx.core.auth.Authorized;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -12,11 +13,14 @@ public class LorqueProcessController {
 
   private final LorqueProcessService service;
 
+  //TODO: auth
+  @Authorized
   @Get("{id}")
   public HttpResponse<?> load(Long id) {
     return HttpResponse.ok(service.load(id));
   }
 
+  @Authorized
   @Get("{id}/status")
   public HttpResponse<?> getStatus(Long id) {
     return HttpResponse.ok(Map.of("status", service.getStatus(id)));
