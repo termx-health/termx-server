8bdb694c7a724ad14ca83d7437801de44d31e305
Roman Bondarev <roman.bondarev@kodality.com>
2024-02-28 14:13:36 +0200

    add privilege name sorting

--- uam/src/main/java/com/kodality/termx/uam/privilege/PrivilegeRepository.java
+++ uam/src/main/java/com/kodality/termx/uam/privilege/PrivilegeRepository.java
@@ -7,9 +7,12 @@ import com.kodality.commons.db.sql.SqlBuilder;
 import com.kodality.commons.model.QueryResult;
 import com.kodality.termx.auth.Privilege;
 import com.kodality.termx.auth.PrivilegeQueryParams;
+import com.kodality.termx.core.auth.SessionInfo;
+import com.kodality.termx.core.auth.SessionStore;
 import io.micronaut.core.util.StringUtils;
 import jakarta.inject.Singleton;
 import java.util.Map;
+import java.util.function.Function;
 
 @Singleton
 public class PrivilegeRepository extends BaseRepository {
@@ -33,7 +36,10 @@ public class PrivilegeRepository extends BaseRepository {
     }, p -> {
       SqlBuilder sb = new SqlBuilder("select * from uam.privilege where sys_status = 'A'");
       sb.append(filter(params));
-      sb.append(order(params, Map.of("code", "code")));
+      sb.append(order(params, Map.<String, Function<String, SqlBuilder>>of(
+          "code", v -> new SqlBuilder("code"),
+          "names", v -> new SqlBuilder("names ->> ?", SessionStore.get().map(SessionInfo::getLang).orElse("en"))
+      )));
       sb.append(limit(params));
       return getBeans(sb.getSql(), bp, sb.getParams());
     });
