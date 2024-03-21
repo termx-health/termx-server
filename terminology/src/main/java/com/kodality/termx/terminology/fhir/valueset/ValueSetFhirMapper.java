ada53fc22d0712476872abbd6e19faa12c8a209f
Marina Ivanova <marina.ivanova@kodality.com>
2024-03-21 14:04:56 +0200

    vs fhir mapper improvement

--- terminology/src/main/java/com/kodality/termx/terminology/fhir/valueset/ValueSetFhirMapper.java
+++ terminology/src/main/java/com/kodality/termx/terminology/fhir/valueset/ValueSetFhirMapper.java
@@ -205,15 +205,16 @@ public class ValueSetFhirMapper extends BaseFhirMapper {
     }
     return rules.stream().filter(r -> r.getType().equals(type)).map(rule -> {
       ValueSetComposeInclude include = new ValueSetComposeInclude();
+      String version = rule.getCodeSystemVersion() == null ? null : StringUtils.isNotEmpty(rule.getCodeSystemVersion().getUri()) ? rule.getCodeSystemVersion().getUri():rule.getCodeSystemVersion().getVersion();
       if (rule.getCodeSystemBaseUri() != null) {
         String[] baseCS = PipeUtil.parsePipe(rule.getCodeSystemBaseUri());
         include.setSystem(baseCS[0]);
         include.setVersion(baseCS.length > 1 ? baseCS[1] : null);
-        fhirValueSet.addExtension(new Extension("http://hl7.org/fhir/StructureDefinition/valueset-supplement").setValueCanonical(rule.getCodeSystemUri()));
+        fhirValueSet.addExtension(new Extension("http://hl7.org/fhir/StructureDefinition/valueset-supplement").setValueCanonical(PipeUtil.toPipe(rule.getCodeSystemUri(), version)));
         include.setConcept(toFhirConcept(rule.getConcepts(), conceptService.query(new ConceptQueryParams().setCodeSystem(rule.getCodeSystem()).setCodeSystemVersion(rule.getCodeSystemVersion().getVersion()).all()).getData()));
       } else {
         include.setSystem(rule.getCodeSystemUri());
-        include.setVersion(rule.getCodeSystemVersion() == null ? null : Optional.ofNullable(rule.getCodeSystemVersion().getUri()).orElse(rule.getCodeSystemVersion().getVersion()));
+        include.setVersion(version);
         include.setConcept(toFhirConcept(rule.getConcepts()));
       }
       include.setFilter(toFhirFilter(rule.getFilters()));
