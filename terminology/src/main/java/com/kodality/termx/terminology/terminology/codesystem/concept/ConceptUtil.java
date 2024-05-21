92fadbb2ca53a8fdd25b8b1ac3c3263453472ab7
Marina Ivanova <marina.ivanova@kodality.com>
2024-05-21 13:48:15 +0300

    concept display: filter out retired designations

--- terminology/src/main/java/com/kodality/termx/terminology/terminology/codesystem/concept/ConceptUtil.java
+++ terminology/src/main/java/com/kodality/termx/terminology/terminology/codesystem/concept/ConceptUtil.java
@@ -1,15 +1,20 @@
 package com.kodality.termx.terminology.terminology.codesystem.concept;
 
+import com.kodality.termx.ts.PublicationStatus;
 import com.kodality.termx.ts.codesystem.Designation;
 import io.micronaut.core.util.CollectionUtils;
 import io.micronaut.core.util.StringUtils;
+import java.util.Comparator;
 import java.util.List;
 
 public class ConceptUtil {
   private static final String DISPLAY = "display";
 
   public static Designation getDisplay(List<Designation> designations, String preferredLang, List<String> otherLang) {
-    List<Designation> displays = designations.stream().filter(d -> DISPLAY.equals(d.getDesignationType())).toList();
+    List<Designation> displays = designations.stream()
+        .filter(d -> DISPLAY.equals(d.getDesignationType()) && !PublicationStatus.retired.equals(d.getStatus()))
+        .sorted(Comparator.comparing(Designation::isPreferred))
+        .toList();
     return displays.stream().filter(d -> StringUtils.isNotEmpty(preferredLang) && d.getLanguage() != null && d.getLanguage().equals(preferredLang))
         .findFirst().orElse(displays.stream()
             .filter(d -> StringUtils.isNotEmpty(preferredLang) && d.getLanguage() != null && d.getLanguage().startsWith(preferredLang))
