0b28a370f91e6d7cc5e4f1605d77721f76ae319b
Marina Ivanova <marina.ivanova@kodality.com>
2024-05-22 13:50:10 +0300

    cs lookup fix

--- terminology/src/main/java/com/kodality/termx/terminology/fhir/codesystem/operations/CodeSystemLookupOperation.java
+++ terminology/src/main/java/com/kodality/termx/terminology/fhir/codesystem/operations/CodeSystemLookupOperation.java
@@ -7,17 +7,17 @@ import com.kodality.kefhir.core.api.resource.TypeOperationDefinition;
 import com.kodality.kefhir.core.exception.FhirException;
 import com.kodality.kefhir.core.model.ResourceId;
 import com.kodality.kefhir.structure.api.ResourceContent;
-import com.kodality.termx.terminology.Privilege;
-import com.kodality.termx.core.auth.SessionStore;
-import com.kodality.termx.core.fhir.BaseFhirMapper;
 import com.kodality.termx.terminology.fhir.codesystem.CodeSystemFhirMapper;
 import com.kodality.termx.terminology.terminology.codesystem.CodeSystemService;
+import com.kodality.termx.terminology.terminology.codesystem.concept.ConceptService;
+import com.kodality.termx.terminology.terminology.codesystem.concept.ConceptUtil;
 import com.kodality.termx.ts.codesystem.CodeSystem;
-import com.kodality.termx.ts.codesystem.CodeSystemAssociation;
+import com.kodality.termx.ts.codesystem.CodeSystemEntityVersion;
 import com.kodality.termx.ts.codesystem.CodeSystemQueryParams;
+import com.kodality.termx.ts.codesystem.CodeSystemVersionReference;
 import com.kodality.termx.ts.codesystem.Concept;
+import com.kodality.termx.ts.codesystem.ConceptQueryParams;
 import com.kodality.termx.ts.codesystem.Designation;
-import com.kodality.termx.ts.codesystem.EntityProperty;
 import com.kodality.termx.ts.codesystem.EntityPropertyType;
 import com.kodality.termx.ts.codesystem.EntityPropertyValue;
 import com.kodality.zmei.fhir.FhirMapper;
@@ -30,15 +30,15 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.List;
-import java.util.stream.Stream;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.collections4.CollectionUtils;
 import org.hl7.fhir.r4.model.ResourceType;
 import org.hl7.fhir.r5.model.OperationOutcome.IssueType;
 
 @Singleton
 @RequiredArgsConstructor
 public class CodeSystemLookupOperation implements InstanceOperationDefinition, TypeOperationDefinition {
+  private final ConceptService conceptService;
   private final CodeSystemService codeSystemService;
 
   public String getResourceType() {
@@ -55,7 +55,7 @@ public class CodeSystemLookupOperation implements InstanceOperationDefinition, T
     String[] parts = CodeSystemFhirMapper.parseCompositeId(id.getResourceId());
     String csId = parts[0];
     String versionNumber = parts[1];
-    Parameters resp = run(csId, null, versionNumber, req);
+    Parameters resp = run(csId, versionNumber, req);
     return new ResourceContent(FhirMapper.toJson(resp), "json");
   }
 
@@ -65,68 +65,58 @@ public class CodeSystemLookupOperation implements InstanceOperationDefinition, T
         .orElseThrow(() -> new FhirException(400, IssueType.INVALID, "system parameter required"));
     String version = req.findParameter("version").map(ParametersParameter::getValueString).orElse(null);
 
-    Parameters resp = run(null, system, version, req);
+    String csId = codeSystemService.query(new CodeSystemQueryParams().setUri(system).limit(1)).findFirst().map(CodeSystem::getId)
+        .orElseThrow(() -> new FhirException(404, IssueType.NOTFOUND, "CodeSystem not found"));
+    Parameters resp = run(csId, version, req);
     return new ResourceContent(FhirMapper.toJson(resp), "json");
   }
 
-  private Parameters run(String csId, String system, String version, Parameters req) {
+  private Parameters run(String csId, String version, Parameters req) {
     String code = req.findParameter("code").map(ParametersParameter::getValueString)
         .orElseThrow(() -> new FhirException(400, IssueType.INVALID, "code parameter required"));
     LocalDate date = req.findParameter("date").map(pp -> LocalDateTime.parse(pp.getValueString()).toLocalDate()).orElse(null);
-    List<String> properties = req.getParameter().stream().filter(p -> "property".equals(p.getName())).map(ParametersParameter::getValueString).toList();
 
-    CodeSystemQueryParams csParams = new CodeSystemQueryParams()
-        .setId(csId)
-        .setConceptCode(code)
-        .setConceptCodeSystemVersion(version)
-        .setUri(system)
-        .setVersionVersion(version)
-        .setVersionReleaseDateGe(date)
-        .setVersionExpirationDateLe(date)
-        .setVersionsDecorated(true).setConceptsDecorated(true).setPropertiesDecorated(true)
-        .setPermittedIds(SessionStore.require().getPermittedResourceIds(Privilege.CS_VIEW))
+
+
+    ConceptQueryParams cQueryParams = new ConceptQueryParams()
+        .setCodeSystem(csId)
+        .setCodeEq(code)
+        .setCodeSystemVersion(version)
+        .setCodeSystemVersionReleaseDateGe(date)
+        .setCodeSystemVersionExpirationDateLe(date)
         .limit(1);
-    CodeSystem cs = codeSystemService.query(csParams).findFirst()
+    Concept c = conceptService.query(cQueryParams).findFirst()
         .orElseThrow(() -> new FhirException(404, IssueType.NOTFOUND, "Concept not found"));
 
     Parameters resp = new Parameters();
-    resp.addParameter(new ParametersParameter().setName("name")
-        .setValueString(cs.getId()));
-    resp.addParameter(new ParametersParameter().setName("version")
-        .setValueString(CollectionUtils.isEmpty(cs.getVersions()) ? null : cs.getVersions().get(0).getVersion()));
+    resp.addParameter(new ParametersParameter().setName("name").setValueString(c.getCodeSystem()));
 
-    String preferredLanguage = CollectionUtils.isEmpty(cs.getVersions()) ? null : cs.getVersions().get(0).getPreferredLanguage();
-    Designation display = getDesignations(cs).filter(d -> d.getDesignationType().equals("display") && (preferredLanguage == null || preferredLanguage.equals(d.getLanguage()))).findFirst().orElse(null);
+
+    Optional<CodeSystemVersionReference> csVersion = c.getVersions().stream().findFirst().flatMap(v -> Optional.ofNullable(v.getVersions()).orElse(List.of()).stream().findFirst());
+    if (csVersion.isPresent()) {
+      resp.addParameter(new ParametersParameter().setName("version").setValueString(csVersion.map(CodeSystemVersionReference::getVersion).orElse(null)));
+    }
+
+    String preferredLanguage = csVersion.map(CodeSystemVersionReference::getPreferredLanguage).orElse(null);
+    List<Designation> designations = c.getVersions().stream().findFirst().map(CodeSystemEntityVersion::getDesignations).orElse(List.of());
+    Designation display = ConceptUtil.getDisplay(designations, preferredLanguage, List.of());
     resp.addParameter(new ParametersParameter().setName("display").setValueString(display != null ? display.getName() : null));
-    getDesignations(cs).filter(d -> display == null || !d.getId().equals(display.getId())).forEach(d -> {
+    designations.stream().filter(d -> display != d).forEach(d -> {
       resp.addParameter(new ParametersParameter("designation")
           .addPart(new ParametersParameter("use").setValueCoding(new Coding(d.getDesignationType())))
           .addPart(new ParametersParameter("value").setValueString(d.getName()))
           .addPart(new ParametersParameter("language").setValueString(d.getLanguage()))
       );
     });
-    cs.getProperties().stream().filter(p -> CollectionUtils.isEmpty(properties) || properties.contains(p.getName())).forEach(p -> {
-      findPropertyValues(cs, p).forEach(pv -> resp.addParameter(new ParametersParameter("property")
-          .addPart(new ParametersParameter("code").setValueString(p.getName()))
-          .addPart(new ParametersParameter("description").setValueString(BaseFhirMapper.toFhirName(p.getDescription(), req.getLanguage())))
-          .addPart(toParameter(p.getType(), pv))
-      ));
+
+    List<String> properties = req.getParameter().stream().filter(p -> "property".equals(p.getName())).map(ParametersParameter::getValueString).toList();
+    List<EntityPropertyValue> propertyValues = c.getVersions().stream().findFirst().map(CodeSystemEntityVersion::getPropertyValues).orElse(List.of());
+    propertyValues.stream().filter(pv -> properties.contains(pv.getEntityProperty())).forEach(pv -> {
+      resp.addParameter(new ParametersParameter("property")
+          .addPart(new ParametersParameter("code").setValueString(pv.getEntityProperty()))
+          .addPart(toParameter(pv.getEntityPropertyType(), pv)));
     });
     return resp;
-  }
-
-  private static Stream<Object> findPropertyValues(CodeSystem cs, EntityProperty p) {
-    if (cs.getConcepts() == null || cs.getConcepts().isEmpty() ||
-        cs.getConcepts().get(0).getVersions() == null || cs.getConcepts().get(0).getVersions().isEmpty()) {
-      return Stream.of();
-    }
-    Stream<Object> propertyValues = cs.getConcepts().get(0).getVersions().get(0).getPropertyValues() == null ? Stream.of() :
-        cs.getConcepts().get(0).getVersions().get(0).getPropertyValues().stream()
-        .filter(pv -> pv.getEntityPropertyId().equals(p.getId())).map(EntityPropertyValue::getValue);
-    Stream<Object> associationValues = cs.getConcepts().get(0).getVersions().get(0).getAssociations() == null ||
-        !List.of("is-a", "parent", "partOf", "groupedBy", "classifiedWith").contains(p.getName()) ? Stream.of() :
-        cs.getConcepts().get(0).getVersions().get(0).getAssociations().stream().map(CodeSystemAssociation::getTargetCode);
-    return Stream.concat(propertyValues, associationValues);
   }
 
   private static ParametersParameter toParameter(String type, Object value) {
@@ -150,14 +140,5 @@ public class CodeSystemLookupOperation implements InstanceOperationDefinition, T
       }
     }
     return result;
-  }
-
-  private static Stream<Designation> getDesignations(CodeSystem cs) {
-    return CollectionUtils.isEmpty(cs.getConcepts()) ? Stream.of() : getDesignations(cs.getConcepts().get(0));
-  }
-
-  private static Stream<Designation> getDesignations(Concept c) {
-    return CollectionUtils.isEmpty(c.getVersions()) || c.getVersions().get(0).getDesignations() == null ? Stream.of() :
-        c.getVersions().get(0).getDesignations().stream();
   }
 }
