70db2b52c136ea4dcc5ca88af02ee564fcb21327
Alexander Volkov <alexander.volkov@askend.com>
2024-10-16 19:53:12 +0300

    TERMX-13: StructureDefinition @ Github

--- modeler/src/main/java/com/kodality/termx/modeler/transformationdefinition/TransformationDefinitionUtils.java
+++ modeler/src/main/java/com/kodality/termx/modeler/transformationdefinition/TransformationDefinitionUtils.java
@@ -1,27 +1,47 @@
 package com.kodality.termx.modeler.transformationdefinition;
 
-import com.kodality.commons.util.JsonUtil;
 import com.kodality.termx.modeler.transformationdefinition.TransformationDefinition.TransformationDefinitionResource;
 import com.kodality.termx.modeler.transformationdefinition.TransformationDefinition.TransformationDefinitionResourceReference;
 import lombok.experimental.UtilityClass;
+import org.hl7.fhir.r5.formats.IParser;
+import org.hl7.fhir.r5.formats.JsonParser;
+import org.hl7.fhir.r5.model.StructureMap;
 
-import java.util.Map;
+import java.io.IOException;
+import java.util.List;
 
 @UtilityClass
 public class TransformationDefinitionUtils {
 
-  public static TransformationDefinition createTransformationDefinitionFromJson(String json) {
-    final Map<String, Object> content = JsonUtil.toMap(json);
+  public static TransformationDefinition createTransformationDefinitionFromResource(StructureMap source) throws IOException {
+    final TransformationDefinition td = new TransformationDefinition();
 
-    final TransformationDefinition result = new TransformationDefinition();
-    result.setMapping(
+    // resources (id/refs)
+    addResources(td, source.getStructure());
+
+    // mapping (main data 2Mb)
+    String json = new JsonParser().setOutputStyle(IParser.OutputStyle.PRETTY).composeString(source);
+    td.setMapping(
         new TransformationDefinitionResource()
+            .setName("main")
+            .setType("mapping")
+            .setSource("static")
             .setReference(
                 new TransformationDefinitionResourceReference()
                     .setContent(json)
             )
     );
+    return td;
+  }
 
-    return result;
+  private static void addResources(TransformationDefinition td, List<StructureMap.StructureMapStructureComponent> structure) {
+    td.setResources(structure.stream().map(item ->
+        new TransformationDefinitionResource()
+            .setName(item.getAlias())
+            .setType("definition")
+            .setSource("url")
+            .setReference(new TransformationDefinitionResourceReference()
+                .setResourceUrl(item.getUrl()))
+    ).toList());
   }
 }
--- modeler/src/main/java/com/kodality/termx/modeler/transformationdefinition/providers/SpaceGithubDataTransformationDefinitionFhirHandler.java
+++ modeler/src/main/java/com/kodality/termx/modeler/transformationdefinition/providers/SpaceGithubDataTransformationDefinitionFhirHandler.java
@@ -1,29 +1,41 @@
 package com.kodality.termx.modeler.transformationdefinition.providers;
 
-import com.kodality.commons.util.JsonUtil;
 import com.kodality.termx.core.github.ResourceContentProvider.ResourceContent;
 import com.kodality.termx.core.sys.space.SpaceGithubDataHandler;
 import com.kodality.termx.modeler.transformationdefinition.TransformationDefinition;
+import com.kodality.termx.modeler.transformationdefinition.TransformationDefinition.TransformationDefinitionResource;
+import com.kodality.termx.modeler.transformationdefinition.TransformationDefinition.TransformationDefinitionResourceReference;
 import com.kodality.termx.modeler.transformationdefinition.TransformationDefinitionQueryParams;
 import com.kodality.termx.modeler.transformationdefinition.TransformationDefinitionService;
+import com.kodality.termx.modeler.transformationdefinition.TransformerService;
 import jakarta.inject.Singleton;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.hl7.fhir.r5.model.StructureMap;
+import org.hl7.fhir.r5.model.StructureMap.StructureMapStructureComponent;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import static com.kodality.termx.modeler.github.CompositeIdUtils.CompositeId;
 import static com.kodality.termx.modeler.github.CompositeIdUtils.parseCompositeId;
-import static com.kodality.termx.modeler.transformationdefinition.TransformationDefinitionUtils.createTransformationDefinitionFromJson;
+import static com.kodality.termx.modeler.transformationdefinition.TransformationDefinition.TransformationDefinitionResource.TransformationDefinitionResourceSource.statik;
+import static com.kodality.termx.modeler.transformationdefinition.TransformationDefinitionUtils.createTransformationDefinitionFromResource;
+import static java.util.stream.Collectors.toMap;
 
+@Slf4j
 @Singleton
 @RequiredArgsConstructor
 public class SpaceGithubDataTransformationDefinitionFhirHandler implements SpaceGithubDataHandler {
 
   private final TransformationDefinitionService transformationDefinitionService;
   private final ResourceContentTransformationDefinitionFhirProvider resourceContentProvider;
+  private final TransformerService transformerService;
 
   @Override
   public String getName() {
@@ -61,16 +73,86 @@ public class SpaceGithubDataTransformationDefinitionFhirHandler implements Space
 
       existingDefinition.ifPresentOrElse(
           td -> {
-            // update existing
-            final Map<String, Object> content = JsonUtil.toMap(jsonContent);
-            td.getMapping().getReference().setContent(jsonContent);
+            if (statik.equals(td.getMapping().getSource())) {
+              updateTransferDefinitionFromJson(td, jsonContent);
+              td.getMapping().getReference().setContent(jsonContent);
+            }
+            // test_source - leave it as it is, we can generate it
+            // fhir_resource will be updated during the save call
+            transformationDefinitionService.save(td);
           },
           () -> {
-            // create new
+            // test_source (can be null)
             final TransformationDefinition definition = createTransformationDefinitionFromJson(jsonContent);
-//            transformationDefinitionService.save(definition);
+            definition.setName(compositeId.code());
+            transformationDefinitionService.save(definition);
           }
       );
     });
+  }
+
+  private void updateTransferDefinitionFromJson(TransformationDefinition td, String sourceContent) {
+    final StructureMap structureMap = transformerService.parse(sourceContent);
+
+    // ! we don't have concept maps here
+    // TODO: update concept maps
+
+    updateResources(td, structureMap.getStructure());
+    addNewResources(td, structureMap.getStructure());
+    removeRedundant(td, structureMap.getStructure());
+  }
+
+  private TransformationDefinition createTransformationDefinitionFromJson(String json) {
+    final StructureMap structureMap = transformerService.parse(json);
+    try {
+      return createTransformationDefinitionFromResource(structureMap);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+
+  private static void updateResources(TransformationDefinition td, List<StructureMapStructureComponent> structureComponents) {
+    final Map<String, StructureMapStructureComponent> source = structureComponents.stream()
+        .filter(sc -> td.getResources().stream().anyMatch(r -> r.getName().equals(sc.getAlias())))
+        .collect(toMap(StructureMapStructureComponent::getAlias, Function.identity()));
+    td.getResources().forEach(r -> {
+      if (source.containsKey(r.getName())
+          && r.getSource().equals("url")
+          && r.getReference() == null) {
+        r.setReference(
+            new TransformationDefinitionResourceReference()
+                .setResourceUrl(source.get(r.getName()).getUrl())
+        );
+      }
+    });
+  }
+
+  private static void removeRedundant(TransformationDefinition td, List<StructureMapStructureComponent> structureComponents) {
+    final Predicate<TransformationDefinitionResource> isContainedInSourcePredicate =
+        r -> structureComponents.stream()
+            .anyMatch(sc -> sc.getAlias().equals(r.getName()));
+    td.setResources(td.getResources().stream()
+        .filter(isContainedInSourcePredicate)
+        .toList());
+  }
+
+  private static void addNewResources(TransformationDefinition td, List<StructureMapStructureComponent> structureComponents) {
+    final List<StructureMapStructureComponent> newComponents = structureComponents.stream()
+        .filter(sc -> td.getResources().stream()
+            .noneMatch(r -> r.getName().equals(sc.getAlias()))
+        )
+        .toList();
+    newComponents.forEach(item ->
+        td.getResources().add(
+            new TransformationDefinitionResource()
+                .setName(item.getAlias())
+                .setSource("url")
+                .setType("definition")
+                .setReference(
+                    new TransformationDefinitionResourceReference()
+                        .setResourceUrl(item.getUrl())
+                )
+        ));
   }
 }
