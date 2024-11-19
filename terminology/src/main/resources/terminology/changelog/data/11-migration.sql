f7115e1a4e8f13d7ed40fc87b11c06a0a769402d
Igor Bossenko <igor.bossenko@gmail.com>
2024-11-19 22:55:08 +0200

    Fix default resources

---
+++ terminology/src/main/resources/terminology/changelog/data/11-migration.sql
@@ -0,0 +1,54 @@
+--liquibase formatted sql
+
+--changeset termx:publisher
+update terminology.entity_property_value v
+   set value='"https://termx.org/fhir"'
+ where value='"https://kodality.org/fhir"';
+
+update terminology.designation d
+   set name='TermX'
+ where exists(
+   select *
+    from terminology.code_system_entity_version ev
+         inner join terminology.concept c on c.id = ev.code_system_entity_id and c.code_system='publisher' and c.code='kts'
+    where d.code_system_entity_version_id = ev.id);
+
+update terminology.code_system
+   set uri = replace(uri, 'http://tx.hl7.ee', 'https://termx.org/fhir')
+ where uri like '%tx.hl7.ee%';
+
+update terminology.value_set
+   set uri = replace(uri, 'http://tx.hl7.ee', 'https://termx.org/fhir')
+ where uri like '%tx.hl7.ee%';
+
+update terminology.code_system
+   set publisher = 'termx'
+ where publisher='kts';
+
+update terminology.value_set
+   set publisher = 'termx'
+ where publisher='kts';
+
+update terminology.code_system
+   set publisher = 'termx'
+ where id='publisher';
+--rollback select 1;
+
+
+--changeset termx:fix-versions
+update terminology.code_system_version
+   set version = '5.0.0'
+ where version='5.0.0-cibuild';
+
+update terminology.code_system_version
+   set version = '6.0.0'
+ where version='6.0.0-cibuild';
+
+update terminology.value_set_version
+   set version = '5.0.0'
+ where version='5.0.0-cibuild';
+
+ update terminology.value_set_version
+   set version = '6.0.0'
+ where version='6.0.0-cibuild';
+--rollback select 1;
