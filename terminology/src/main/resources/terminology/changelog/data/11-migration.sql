d26d07d5efbe278e1e924218172c36bc1f4d2c29
Igor Bossenko <igor.bossenko@gmail.com>
2024-12-02 10:47:15 +0200

    Fix changelog, for cases when termx concept already exists

--- terminology/src/main/resources/terminology/changelog/data/11-migration.sql
+++ terminology/src/main/resources/terminology/changelog/data/11-migration.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset termx:publisher
+--changeset termx:publisher-3
 update terminology.entity_property_value v
    set value='"https://termx.org/fhir"'
  where value='"https://kodality.org/fhir"';
@@ -8,10 +8,13 @@ update terminology.entity_property_value v
 update terminology.designation d
    set name='TermX'
  where exists(
-   select *
-    from terminology.code_system_entity_version ev
-         inner join terminology.concept c on c.id = ev.code_system_entity_id and c.code_system='publisher' and c.code='kts'
-    where d.code_system_entity_version_id = ev.id);
+          select 1
+            from terminology.code_system_entity_version ev
+                 inner join terminology.concept c on c.id = ev.code_system_entity_id and c.code_system='publisher' and c.code='kts'
+            where d.code_system_entity_version_id = ev.id)
+    and not exists(
+          select 1 from terminology.concept
+           where code='termx' and code_system='publisher');
 
 update terminology.code_system
    set uri = replace(uri, 'http://tx.hl7.ee', 'https://termx.org/fhir')
@@ -53,12 +56,18 @@ update terminology.value_set_version
  where version='6.0.0-cibuild';
 --rollback select 1;
 
---changeset termx:kts-to-termx
-update terminology.concept
-   set code = 'termx'
- where code='kts' and code_system='publisher';
-
+--changeset termx:kts-to-termx-3
  update terminology.code_system_entity_version ev
    set code = 'termx'
- where code_system='publisher' and code='kts';
+ where code_system='publisher' and code='kts'
+   and not exists(
+      select 1 from terminology.concept
+       where code='termx' and code_system='publisher');
+
+update terminology.concept
+   set code = 'termx'
+ where code='kts' and code_system='publisher'
+   and not exists(
+      select 1 from terminology.concept
+       where code='termx' and code_system='publisher');
 --rollback select 1;
