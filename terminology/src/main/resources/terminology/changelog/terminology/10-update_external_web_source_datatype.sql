21718bdd4565a8d3528410ae4c9b36fd88d7f361
Rainer Randmaa <rainerrandmaa@gmail.com>
2025-09-24 12:14:46 +0300

    amend changeset to include external web source datatype change for ValueSet

--- terminology/src/main/resources/terminology/changelog/terminology/10-update_external_web_source_datatype.sql
+++ terminology/src/main/resources/terminology/changelog/terminology/10-update_external_web_source_datatype.sql
@@ -5,6 +5,10 @@ alter table terminology.code_system
 drop column if exists external_web_source,
 add column external_web_source text;
 
+alter table terminology.value_set
+drop column if exists external_web_source,
+add column external_web_source text;
+
 --rollback  alter table code_system
 --rollback  drop column if exists external_web_source,
 --rollback  add column external_web_source boolean;
