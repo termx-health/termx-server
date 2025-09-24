ab5e988dd9b00861fc2fddf3be35cc39cd7b6472
Rainer Randmaa <rainerrandmaa@gmail.com>
2025-09-24 12:17:10 +0300

    add rollback script to changeset

--- terminology/src/main/resources/terminology/changelog/terminology/10-update_external_web_source_datatype.sql
+++ terminology/src/main/resources/terminology/changelog/terminology/10-update_external_web_source_datatype.sql
@@ -9,6 +9,9 @@ alter table terminology.value_set
 drop column if exists external_web_source,
 add column external_web_source text;
 
---rollback  alter table code_system
+--rollback  alter table terminology.code_system
+--rollback  drop column if exists external_web_source,
+--rollback  add column external_web_source boolean;
+--rollback  alter table terminology.value_set
 --rollback  drop column if exists external_web_source,
 --rollback  add column external_web_source boolean;
