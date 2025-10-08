f1368bebdce243fda5c57c090a10067a5a90df8a
Rainer Randmaa <rainerrandmaa@gmail.com>
2025-10-08 10:16:54 +0300

    VersionReference generic constraint

--- termx-api/src/main/java/com/kodality/termx/ts/VersionReference.java
+++ termx-api/src/main/java/com/kodality/termx/ts/VersionReference.java
@@ -1,10 +1,11 @@
 package com.kodality.termx.ts;
 
+import com.kodality.termx.commons.UniqueResource;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
-public abstract class VersionReference<T> {
+public abstract class VersionReference<T extends VersionReference<T>> {
     private Long id;
     @Pattern(regexp = "[A-Za-z0-9\\-\\.]{1,64}")
     private String version;
