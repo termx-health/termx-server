14f6db4f995854cb77eab4ba019b024d9410372b
Aleksei Lissitsin <aleksei.lissitsin@helex.health>
2026-03-09 14:16:48 +0200

    fix ucum essence db table

--- ucum/src/main/resources/ucum/changelog/ucum/12-ucum_essence.sql
+++ ucum/src/main/resources/ucum/changelog/ucum/12-ucum_essence.sql
@@ -1,21 +1,13 @@
 --liquibase formatted sql
 
---changeset termx:ucum_essence
+--changeset termx:ucum_essence-2
 drop table if exists ucum.essence;
 create table ucum.essence (
   id                     bigserial primary key,
   version                text not null,
   essence_xml            xml not null,
-
-  sys_status            char(1) default 'A' not null,
-  sys_version           integer not null,
-  sys_created_at        timestamp not null,
-  sys_created_by        text not null,
-  sys_modified_at       timestamp,
-  sys_modified_by       text
-    );
-
-select core.create_table_metadata('ucum.essence');
+  sys_status             char(1) default 'A' not null
+  );
 
 create unique index ucum_essence_version_uix on ucum.essence(version) where sys_status = 'A';
 --rollback drop table if exists ucum.essence;
