380979a7d54846c29689921b730e4c7824cdd4f3
Igor Bossenko <igor.bossenko@gmail.com>
2026-05-18 17:11:58 +0300

    chore(liquibase): use termx as the author of the new changeset
    Per project policy, new code should not reference "kodality". The
    changeset hasn't been applied anywhere yet so renaming the author
    token is safe; existing applied changesets are left alone to keep
    their author:id keys stable in deployed databases.

--- terminology/src/main/resources/terminology/changelog/terminology/14-entity_version_effective_time.sql
+++ terminology/src/main/resources/terminology/changelog/terminology/14-entity_version_effective_time.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset kodality:code_system_entity_version_effective_time
+--changeset termx:code_system_entity_version_effective_time
 alter table terminology.code_system_entity_version
     add column if not exists effective_time text;
 --rollback alter table terminology.code_system_entity_version drop column if exists effective_time;
